41d7695b98ef6d6db77408313294e88aa9aefa2d
FAZuH <fazuhhh@proton.me>
2024-11-25 20:57:17 +0700

    ci: ruff and black formatting

--- src/faz/bot/dev/test/fixtures_api_creator.py
+++ src/faz/bot/dev/test/fixtures_api_creator.py
@@ -3,12 +3,11 @@ import json
 
 from loguru import logger
 
+from faz.bot.dev.test._base_wynn_fixtures_api import BaseWynnFixturesApi
 from faz.bot.wynn.api.response.guild_response import GuildResponse
 from faz.bot.wynn.api.response.online_players_response import OnlinePlayersResponse
 from faz.bot.wynn.api.response.player_response import PlayerResponse
 from faz.bot.wynn.api.wynn_api import WynnApi
-
-from faz.bot.dev.test._base_wynn_fixtures_api import BaseWynnFixturesApi
 
 
 class FixturesApiCreator(BaseWynnFixturesApi):
