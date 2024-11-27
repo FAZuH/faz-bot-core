7cbd4d1f878f0e4f4ee58c2eb5482180a3109acf
FAZuH <fazuhhh@proton.me>
2024-11-27 10:05:13 +0700

    feat: Add select as dataframe to GuildHistoryRepository

--- tests/database/fazwynn/test_guild_history_repository.py
+++ tests/database/fazwynn/test_guild_history_repository.py
@@ -1,10 +1,50 @@
+from datetime import datetime, timedelta
 from typing import override
+
+import pandas
 
 from faz.bot.database.fazwynn.repository.guild_history_repository import GuildHistoryRepository
 from tests.database.fazwynn._common_fazwynn_repository_test import CommonFazwynnRepositoryTest
 
 
 class TestGuildHistoryRepository(CommonFazwynnRepositoryTest.Test[GuildHistoryRepository]):
+    async def test_select_between_period_as_dataframe_returns_within_range(
+        self,
+    ) -> None:
+        # Prepare
+        self.__insert_mock_data()
+        since = self._get_mock_datetime() - timedelta(days=100)
+        until = self._get_mock_datetime() + timedelta(days=100)
+        mock = self._get_mock_data()[0]
+
+        # Act
+        res = self.repo.select_between_period_as_dataframe(mock.uuid, since, until)
+
+        # Assert
+        self.assertEqual(len(res), 1)
+        self.assertIsInstance(res, pandas.DataFrame)
+        self.assertEqual(res.iloc[0].to_dict(), mock.to_dict())
+
+    async def test_select_between_period_as_dataframe_returns_empty_when_outside_range(
+        self,
+    ) -> None:
+        # Prepare
+        self.__insert_mock_data()
+        since = datetime.fromtimestamp(0)
+        until = datetime.fromtimestamp(100)
+        mock = self._get_mock_data()[0]
+
+        # Act
+        res = self.repo.select_between_period_as_dataframe(mock.uuid, since, until)
+
+        # Assert
+        self.assertEqual(len(res), 0)
+
+    def __insert_mock_data(self):
+        mock = self._get_mock_data()[0]
+        with self.database.enter_session() as ses:
+            ses.add(mock)
+
     @override
     async def _create_table(self) -> None:
         db = self.database
